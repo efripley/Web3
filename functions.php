2421f19adbfdbea5f3779a7e7ade0b655332ec2c
Ernie Ripley <efripley@gmail.com>
2017-10-10 22:45:14 -0600

    Added buttons to cycle through months in month view

--- functions.php
+++ functions.php
@@ -12,10 +12,26 @@ function buildMonth($items, $year, $month){
   $monthString = DateTime::createFromFormat('!m', $month)->format('F');
   $day = 1;
   $hours = 0;
-  echo "<div class=\"title-cmp\"><span class=\"text\">{$monthString}</span></div>";
-  echo "<div class=\"day-titles\"><span>S</span><span>M</span><span>T</span><span>W</span><span>T</span><span>F</span><span>S</span></div>";
   $dayString = sprintf("%02d", $day);
   $date = "{$year}-{$month}-{$dayString}";
+  $previousMonth = date('Y-m-d', strtotime($date . ' -1 month'));
+  $nextMonth = date('Y-m-d', strtotime($date . ' +1 month'));
+  echo "<div class=\"title-cmp\">
+    <span class=\"text\">";
+  $year = date('Y', strtotime($previousMonth));
+  $month = date('m', strtotime($previousMonth));
+  $dayString = date('d', strtotime($previousMonth));
+  echo "<a class=\"back-arrow\" href=\"{$CONFIG['url']}?view=month&year={$year}&month={$month}&day={$dayString}\">&lsaquo;</a>
+    <span class=\"date-text\">{$monthString}</span>";
+  $year = date('Y', strtotime($nextMonth));
+  $month = date('m', strtotime($nextMonth));
+  $dayString = date('d', strtotime($nextMonth));
+  echo "<a class=\"back-arrow\" href=\"{$CONFIG['url']}?view=month&year={$year}&month={$month}&day={$dayString}\">&rsaquo;</a>
+      </span>
+    </div>";
+  echo "<div class=\"day-titles\"><span>S</span><span>M</span><span>T</span><span>W</span><span>T</span><span>F</span><span>S</span></div>";
+  $year = date('Y', strtotime($date));
+  $month = date('m', strtotime($date));
   $firstDay = date('N', strtotime($date));
   $totalDays = date('t', strtotime($date));
   if($firstDay < 7){
@@ -28,6 +44,7 @@ function buildMonth($items, $year, $month){
       ";
     }
   }
+  $day = 1;
   $item = $items->fetch_assoc();
   while(true){
     $dayString = sprintf("%02d", $day);
